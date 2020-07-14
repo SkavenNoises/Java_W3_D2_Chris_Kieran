7b761e629c2ea15db3d043b6760fabb396bda633
Aki <kieran.mortimer@gmail.com>
2020-07-14 09:50:03 +0200

    Init src structure on kieran branch, added first WIP of first exercise

---
+++ src/Exercise1/Main.java
@@ -0,0 +1,10 @@
+package Exercise1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Author author = new Author("abs", "abc@abc.com", 'm', "instragram.com/author");
+
+		System.out.println(author.toString());
+	}
+}
