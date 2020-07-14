b63512f33fa4f46c9fdc1132ba308f30595b3b2d
Aki <kieran.mortimer@gmail.com>
2020-07-14 10:03:45 +0200

    Exercise 1

--- src/Exercise1/Main.java
+++ src/Exercise1/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		Author author = new Author("abs", "abc@abc.com", 'm', "instragram.com/author");
-
 		System.out.println(author.toString());
+
+		Book book = new Book("TestBook", author, 25.99);
+		System.out.println(book.toString());
 	}
 }
